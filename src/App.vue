c850fe9891f3c043c6f5797d8985bff9026b1d80
matsuihidetoshi <partyrockeverydayimshufflin@gmail.com>
2019-05-11 20:20:58 +0900

    copied original

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">    
+    <Square msg="Number Test" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Square from './components/Square.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Square
   }
 }
 </script>
@@ -23,6 +22,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 30px;
 }
 </style>
